07b8af8f155ba2f72bae19e090afcc66fb2061c8
Mariana Caradec <maricaradec@gmail.com>
2025-07-11 15:48:13 -0300

    feat: created registration page

---
+++ src/pages/Registration.tsx
@@ -0,0 +1,53 @@
+import { useAuthContext } from "../contexts/AuthContext";
+
+const Registration = () => {
+  const { registrationInputValue, setRegistrationInputValue, registerHandler } =
+    useAuthContext();
+
+  const handleRegisterSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await registerHandler();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleRegisterSubmit}>
+        <input
+          type="email"
+          value={registrationInputValue.email}
+          onChange={(e) =>
+            setRegistrationInputValue({
+              ...registrationInputValue,
+              email: e.target.value,
+            })
+          }
+          required
+        />
+        <input
+          type="password"
+          value={registrationInputValue.password}
+          onChange={(e) =>
+            setRegistrationInputValue({
+              ...registrationInputValue,
+              password: e.target.value,
+            })
+          }
+          required
+        />
+        <input
+          type="text"
+          value={registrationInputValue.roleName}
+          onChange={(e) =>
+            setRegistrationInputValue({
+              ...registrationInputValue,
+              roleName: e.target.value,
+            })
+          }
+        />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
+
+export default Registration;
