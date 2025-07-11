13156356a178b2c66464e94b01af77b6cb6b297b
Mariana Caradec <maricaradec@gmail.com>
2025-07-11 15:48:57 -0300

    feat: created login page -didn't test it yet

---
+++ src/pages/LogIn.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useAuthContext } from "../contexts/AuthContext";
+
+const LogIn = () => {
+  const { loginInputValue, setLoginInputValue, loginHandler } =
+    useAuthContext();
+
+  const handleLoginSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await loginHandler();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLoginSubmit}>
+        <input
+          type="email"
+          value={loginInputValue.email}
+          onChange={(e) =>
+            setLoginInputValue({ ...loginInputValue, email: e.target.value })
+          }
+          required
+        />
+        <input
+          type="password"
+          value={loginInputValue.password}
+          onChange={(e) =>
+            setLoginInputValue({
+              ...loginInputValue,
+              password: e.target.value,
+            })
+          }
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default LogIn;
