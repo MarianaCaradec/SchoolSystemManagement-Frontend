d64819fbf832dc051197b208357525700c4ea89d
Mariana Caradec <maricaradec@gmail.com>
2025-07-11 15:44:11 -0300

    feat: added fetch for registration and login; removed userId from student fetch

--- src/api/apiHelpers.ts
+++ src/api/apiHelpers.ts
@@ -1,9 +1,55 @@
 import api from "../api/baseCall";
 
+export const register = async (email: string, password: string, roleName: string) => {
+    try {
+    console.log(`[🔗] POST a http://localhost:5204/api/auth/register`);
+    console.log("Payload:", { email, password, roleName });
+        const response = await api.post("api/auth/register", {
+            email,
+            password,
+            roleName    
+        }, {
+            withCredentials: true
+        });
+        return response.data || null;
+    } catch (error) {
+        console.error("Error during registration:", error);
+        return null;
+    }
+}
+
+export const login = async (email: string, password: string) => {
+    try {
+        const response = await api.post("api/auth/login", {
+            email,
+            password
+        }, {
+            withCredentials: true
+        });
+        return response.data || null;
+    } catch (error) {
+        console.error("Error during login:", error);
+        return null;
+    }
+}
+
+export const logout = async () => {
+    try {
+        const response = await api.post("api/auth/logout", {}, 
+            {
+            withCredentials: true
+        });
+        return response.data || null;
+    } catch (error) {
+        console.error("Error during logout:", error);
+        return null;
+    }
+}
+
 export const getStudents = async () => {
     try {
-    const response = await api.get(`/api/student?userId=7`);
-    return response.data || [];
+        const response = await api.get(`/api/student`);
+        return response.data || [];
     } catch (error) {
         console.error("Error calling students data:", error);
         return null;
@@ -12,7 +58,7 @@ export const getStudents = async () => {
 
 export const getStudentById = async (studentId: number) => {
     try {
-        const response = await api.get(`/api/student/${studentId}?userId=7`)
+        const response = await api.get(`/api/student/${studentId}`)
         return response.data || null;
     } catch (error) {
         console.error("Error calling student by ID:", error);
