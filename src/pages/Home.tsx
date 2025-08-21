8d5d1a80f2c949105604083ecd4f8f1726bb0403
Mariana Caradec <maricaradec@gmail.com>
2025-08-21 13:27:47 -0300

    feat: added landing page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>School System Management</h1>
+      <p>Welcome to the School System Management application!</p>
+      <p>Please log in or register to continue.</p>
+      <p>
+        If you are a new user, please complete your profile after registration
+        so we can assing you to the proper dashboard.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
