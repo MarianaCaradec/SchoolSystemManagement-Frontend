0be10001ebca2aa405cb822d65d1b00a4541903b
Mariana Caradec <maricaradec@gmail.com>
2025-08-21 22:16:00 -0300

    feat: added dashboards' paths by role

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Registration from "./pages/Registration";
 import CompleteProfile from "./pages/CompleteProfile";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import StudentDashboard from "./pages/StudentDashboard";
 
 function App() {
   return (
@@ -15,9 +16,18 @@ function App() {
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/complete-profile" element={<CompleteProfile />} />
-            <Route path="/login" element={<LogIn />} />
             <Route path="/register" element={<Registration />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/complete-profile" element={<CompleteProfile />} />
+            <Route path="/student/dashboard" element={<StudentDashboard />} />
+            <Route
+              path="/teacher/dashboard"
+              element={<h1>Welcome to the Teacher Dashboard!</h1>}
+            />
+            <Route
+              path="/admin/dashboard"
+              element={<h1>Welcome to the Admin Dashboard!</h1>}
+            />
           </Routes>
         </AuthContextProvider>
       </BrowserRouter>
