45931c7d6ac3d009c3a87f998fb20d1ea9553503
Mariana Caradec <maricaradec@gmail.com>
2025-09-04 10:46:46 -0300

    feat: added teacher profile creation and update of user, and improved order with comments

--- src/api/apiHelpers.ts
+++ src/api/apiHelpers.ts
@@ -1,6 +1,7 @@
 import api from "../api/baseCall";
 import type { StudentForm, TeacherForm } from "../utils/types";
 
+// Register a new user
 export const register = async (email: string, password: string, roleName: string) => {
     try {
         const response = await api.post("api/auth/register", {
@@ -18,34 +19,7 @@ export const register = async (email: string, password: string, roleName: string
     }
 }
 
-export const createStudentProfile = async (studentData: StudentForm) => {
-    try {
-        const response = await api.post("api/student", 
-            studentData, 
-            {withCredentials: true}
-        );
-
-        return response.data || null;
-    } catch (error) {
-        console.error("Error creating student profile:", error);
-        return null;
-    }
-}
-
-export const createTeacherProfile = async (teacherData: TeacherForm) => {
-    try {
-        const response = await api.post("api/teacher", 
-            teacherData, 
-            {withCredentials: true}
-        );
-
-        return response.data || null;
-    } catch (error) {
-        console.error("Error creating teacher profile:", error);
-        return null;
-    }
-}
-
+// Login and logout an existing user
 export const login = async (email: string, password: string) => {
     try {
         const response = await api.post("api/auth/login", {
@@ -76,6 +50,55 @@ export const logout = async () => {
     }
 }
 
+// Create profile fot student and teacher
+export const createStudentProfile = async (studentData: StudentForm) => {
+    try {
+        const response = await api.post("api/student", 
+            studentData, 
+            {withCredentials: true}
+        );
+
+        return response.data || null;
+    } catch (error) {
+        console.error("Error creating student profile:", error);
+        return null;
+    }
+}
+
+export const createTeacherProfile = async (teacherData: TeacherForm) => {
+    try {
+        const response = await api.post("api/teacher", 
+            teacherData, 
+            {withCredentials: true}
+        );
+
+        return response.data || null;
+    } catch (error) {
+        console.error("Error creating teacher profile:", error);
+        return null;
+    }
+}
+
+// Update user, student, teacher, class, attendance, grade
+export const updateUser = async (userId: number, userEmail: string, userPassword: string, userRoleName: string) => {
+    try {
+        const response = await api.patch(`api/user/${userId}`, 
+            {
+                email: userEmail ? userEmail : "",
+                password: userPassword ? userPassword : "",
+                roleName: userRoleName ? userRoleName : ""
+            },
+            {withCredentials: true}
+        );
+        
+        return response.data || null;
+    } catch (error) {
+        console.error("Error updating user", error);
+        return null;
+    }
+}
+
+// Get all users, students, teachers, classes, attendances, grades
 export const getStudents = async () => {
     try {
         const response = await api.get(`/api/student`);
@@ -87,6 +110,7 @@ export const getStudents = async () => {
     }
 };
 
+// Get by id student, teacher, class, attendance, grade
 export const getStudentById = async (studentId: number) => {
     try {
         const response = await api.get(`/api/student/${studentId}`)
